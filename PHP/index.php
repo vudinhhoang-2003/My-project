f78d5690c5c694e0409f33ab0d8bce4e7b7ae0da
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-07 19:13:13 +0700

    Option IT

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,32 @@
-<?php
-echo "chao vu dinh hoang - 2121050409";
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title> Log in to HUMG</title>
+  </head>
+  <body>
+    <form action="" method="post" name='register'>
+      <h1>Lựa chọn chuyên ngành</h1>
+      <select name="industry">
+        <option value="CNTTDH"> CNTT địa học</option>
+        <option value="CNPM">Công nghệ phần mềm</option>
+        <option value="KHMT">Khoa học máy tính</option>
+        <option value="THKT">Tin học kinh tế</option>
+        <option value="HTTT">Hệ thống thông tin</option>
+      </select><br>
+
+      <h2>Lựa chọn học phần</h2>
+      <select name="subjects" size="4" multiple>  <!-- multiple có thể nhập nhiều giá trị-->
+        <option value="web">Phát triển ứng dụng Web</option>
+        <option value="python">Python</option>
+        <option value="web-py">Phát triển ứng dụng Web với Python</option>
+        <option value="cslt">Cơ sở lập trình</option>
+      </select><br>
+
+      <h2>Ý kiến phản hồi</h2>
+      <textarea name="message" rows="15" cols="35">
+      </textarea><br>
+      <input type="submit" name="submit" value="Submit" />
+    </form>
+  </body>
+</html>

27d7033b3a32f2d6a59558d4230907da75b6be7c
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-08 15:04:55 +0700

    Slide 25

--- PHP/index.php
+++ PHP/index.php
@@ -1,8 +1,11 @@
 <?php
-  $longAccount = 10000000;
-  echo "Số tiền trong tài khoản Long: $longAccount". "<br>";
-  $HUMGPay = 10000000;
-  echo "HUMG chuyển tiền: $HUMGPay"."<br>";
-  $longAccount = $longAccount + $HUMGPay;
-  echo "Số tiền trong tài khoản Long: $longAccount";
+$Subject = 'Lập trình ứng dụng web';
+$universityName = 'HUMG';
+$firstNumber = 2;
+$secondtNumber = 2;
+
+echo "Học ".$Subject. "ở phòng 502 tại trường" . $universityName."<br>";
+echo "Học $Subject ở phòng 502 tại trường $universityName <br>";
+echo "<h1> $universityName </h1>";
+echo $firstNumber + $secondtNumber;
 ?>

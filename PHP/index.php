23282507e86c9c687bfcca616fbc1daa9744315e
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-08 15:02:54 +0700

    Slide 24

--- PHP/index.php
+++ PHP/index.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>VŨ ĐÌNH HOÀNG - 2121050409</title>
-  </head>
-  <body>
-    <?php
-      echo "MY FIRST PHP FILE";
-    ?>
-    <h3>Hello World!</h3>
-    <select>
-      <option>1</option>
-      <option>2</option>
-    </select>
-  </body>
-</html>
+<?php
+  $longAccount = 10000000;
+  echo "Số tiền trong tài khoản Long: $longAccount". "<br>";
+  $HUMGPay = 10000000;
+  echo "HUMG chuyển tiền: $HUMGPay"."<br>";
+  $longAccount = $longAccount + $HUMGPay;
+  echo "Số tiền trong tài khoản Long: $longAccount";
+?>

031207745bdae6e0c19ed339ddf01a856278ee2a
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-08 14:54:10 +0700

    Slide 17

--- PHP/index.php
+++ PHP/index.php
@@ -1,9 +1,18 @@
-<?php
-  echo "VŨ ĐÌNH HOÀNG - 2121050409 </br>";
-  echo "MY FIRST PHP FILE";
-  echo "<h3>Hello</h3>";
-  echo "<select>";
-  echo "<option>1</option>";
-  echo "<option>2</option>";
-  echo "</select>";
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      echo "VŨ ĐÌNH HOÀNG - 2121050409 </br>";
+      echo "MY FIRST PHP FILE";
+      echo "<h3>Hello</h3>";
+      echo "<select>";
+      echo "<option>1</option>";
+      echo "<option>2</option>";
+      echo "</select>";
+    ?>
+  </body>
+</html>

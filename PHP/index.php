42b5b61d32f469087718f04cfaf4aa51b069f36f
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-08 15:31:53 +0700

    Prac 02 Slide 30

--- PHP/index.php
+++ PHP/index.php
@@ -1,10 +1,19 @@
 <?php
-  $firstNumber = 79;
-  $secondNumber = 80;
+  $a = 1;
+  $b = 6;
+  $c = 9;
 
-  if ($firstNumber > $secondNumber) {
-    echo "Số lớn nhất là: $firstNumber";
-  } else {
-    echo "Số lớn nhất là: $secondNumber";
+  echo "Phương trình: $a.x**2 + $b.x + $c"."<br>";
+
+  $delta = $b*$b - 4*$a*$c;
+
+  if ($delta > 0) {
+    echo "Phương trình có 2 nghiệm";
+  }
+  elseif ($delta = 0) {
+    echo "Phương trình có nghiệm kép";
+  }
+  else {
+    echo "Phương trình vô nghiệm";
   }
 ?>

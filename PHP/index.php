316f0473717b31f1dc5c89f8dabb1843648f30b4
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-08 15:56:41 +0700

    Prac 04 Slide 30

--- PHP/index.php
+++ PHP/index.php
@@ -1,19 +1,16 @@
+
+<!-- VŨ ĐÌNH HOÀNG - 2121050409 -->
+
+
 <?php
-  $a = 1;
-  $b = 6;
-  $c = 9;
+  $a = 5;
+  $b = 3;
 
-  echo "Phương trình: $a.x**2 + $b.x + $c"."<br>";
-
-  $delta = $b*$b - 4*$a*$c;
-
-  if ($delta > 0) {
-    echo "Phương trình có 2 nghiệm";
-  }
-  elseif ($delta = 0) {
-    echo "Phương trình có nghiệm kép";
+  if ($a == $b) {
+    echo "Tứ giác là hình vuông";
   }
   else {
-    echo "Phương trình vô nghiệm";
+    echo "Tứ giác là hình chữ nhật";
   }
+
 ?>

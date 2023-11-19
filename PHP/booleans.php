90bd95d89c4524b4e8eca9d8a3b4589767b3118e
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-20 00:58:49 +0700

    Slide 2

---
+++ PHP/booleans.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>VŨ ĐÌNH HOÀNG - 2121050409</title>
+  </head>
+  <body>
+    <?php
+      $T = True;
+      $F = False;
+      echo "T = $T <br>";
+      echo "F = $F <br>";
+
+      $check1 = 10 > 5;
+      $check2 = 20 < 10;
+      $check3 = $check1 && $check2;
+      $check4 = $check1 || $check2;
+      echo "check1 = $check1 <br>";
+      echo "check2 = $check2 <br>";
+      echo "check3 = $check3 <br>";
+      echo "check4 = $check4";
+     ?>
+  </body>
+</html>

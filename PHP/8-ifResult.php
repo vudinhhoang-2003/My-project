66ae3fb4be1c636afc77f8914f5cc0626640122c
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-20 23:53:52 +0700

    Slide 8 & 10 & 14

---
+++ PHP/8-ifResult.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+        echo "THE GAME";
+        if ($_GET['response'] == 'yes')
+        {
+          echo "HAS BEEN QUITED";
+        }
+        else
+        {
+          echo "WILL BE CONTINUED IN 3 SECONDS";
+        }
+        echo "<br>AFTER IF STATEMENT";
+     ?>
+
+  </body>
+</html>

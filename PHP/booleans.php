5f98ff4e18345a3798d0f738bc4c99225a15712e
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-20 23:39:05 +0700

    Slide 10

--- PHP/booleans.php
+++ PHP/booleans.php
@@ -1,18 +1,22 @@
-!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Vũ Đình Hoàng - 2121050409</title>
   </head>
   <body>
-    <h3>QUIT THE GAME</h3>
-    <form class="" action="ifResult.php" method="get">
-      <select  name="response">
-        <option value="yes">YES</option>
-        <option value="no">NO</option>
-      </select>
-      <input type="submit" name="" value="Submit">
-    </form>
+    <?php
+        echo "THE GAME";
+        if ($_GET["response"] == "yes")
+        {
+          echo "HAS BEEN QUITED";
+        }
+        else
+        {
+          echo "WILL BE CONTINUED IN 3 SECONDS";
+        }
+        echo "<br>AFTER IF STATEMENT";
+     ?>
 
   </body>
 </html>

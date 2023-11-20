50d7830325bdf454f67c9f714ea5617b9b7b976c
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-20 23:35:45 +0700

    Slide 8

--- PHP/booleans.php
+++ PHP/booleans.php
@@ -1,24 +1,18 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>VŨ ĐÌNH HOÀNG - 2121050409</title>
+    <title>Vũ Đình Hoàng - 2121050409</title>
   </head>
   <body>
-    <?php
-      $T = True;
-      $F = False;
-      echo "T = $T <br>";
-      echo "F = $F <br>";
+    <h3>QUIT THE GAME</h3>
+    <form class="" action="ifResult.php" method="get">
+      <select  name="response">
+        <option value="yes">YES</option>
+        <option value="no">NO</option>
+      </select>
+      <input type="submit" name="" value="Submit">
+    </form>
 
-      $check1 = 10 > 5;
-      $check2 = 20 < 10;
-      $check3 = $check1 && $check2;
-      $check4 = $check1 || $check2;
-      echo "check1 = $check1 <br>";
-      echo "check2 = $check2 <br>";
-      echo "check3 = $check3 <br>";
-      echo "check4 = $check4";
-     ?>
   </body>
 </html>

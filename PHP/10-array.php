56b53fa9cd1eb3bd5f7a596ebef9b6ed668345b8
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-22 16:00:34 +0700

    Slide 26

---
+++ PHP/10-array.php
@@ -0,0 +1,9 @@
+<?php
+    echo "Vũ Đình Hoàng - 2121050409";
+    $exchangeRate = array(22300, 27300, 17000, 120);
+    echo "<h3>Exchange rate to VND</h3>";
+    echo "USD: $exchangeRate[0]<br>";
+    echo "EUR: $exchangeRate[1]<br>";
+    echo "SGD: $exchangeRate[2]<br>";
+    echo "JPY: $exchangeRate[3]<br>";
+?>

7612ec86ee64426d21d212ecc0abbd53691dd379
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-22 15:22:54 +0700

    Slide 23

---
+++ PHP/prac_23.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $weight = $_GET['weight'];
+      $height = $_GET['height'];
+
+      if ($_GET['gender'] == 'Nam') {
+        if ($height > 1.6 && $weight >= 50) {
+          echo "bạn đạt yêu cầu vào công ty";
+        }
+        else {
+          echo "bạn bị loại";
+        }
+      }
+      elseif ($_GET['gender'] == 'Nu') {
+        if ($height > 1.5 && 40 <= $weight && $weight <= 70) {
+          echo "bạn đạt yêu cầu vào công ty";
+        }
+        else {
+          echo "bạn bị loại";
+        }
+      }
+    ?>
+  </body>
+</html>

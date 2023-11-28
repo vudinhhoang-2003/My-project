ff8dcabd249c0afc6403a35e45a9edb9023a05ca
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-28 19:16:08 +0700

    function slide 23

--- PHP/function.php
+++ PHP/function.php
@@ -5,15 +5,7 @@
       {
         echo "<img src='Images/$item'
         width = 50px height = 50px alt = '$item'>";
-
       }
     }
-    $items = array('1.jpg', '2.jpg', '3.jpg');
-    dislay_image_list($items);
 ?>
 <h2> Vũ Đình Hoàng - 2121050409</h2>
-<h1>function</h1>
-<?php
-    $items = array('2.jpg', '3.jpg');
-    dislay_image_list($items);
-?>

d57ea3bcd68c3e18c7555b0ed8edf99275bf21f8
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-28 18:57:50 +0700

    function slide 21

--- PHP/function.php
+++ PHP/function.php
@@ -1,18 +1,19 @@
 <?php
-    $items = array('1.jpg', '2.jpg', '3.jpg');
-    foreach ($items as $item)
+    function dislay_image_list($imageList)
     {
-      echo "<img src='Images/$item' width = 50px height = 50px alt='$item'>";
-      // code...
+      foreach ($imageList as $item)
+      {
+        echo "<img src='Images/$item'
+        width = 50px height = 50px alt = '$item'>";
+
+      }
     }
- ?>
+    $items = array('1.jpg', '2.jpg', '3.jpg');
+    dislay_image_list($items);
+?>
 <h2> Vũ Đình Hoàng - 2121050409</h2>
- <h1>function</h1>
- <?php
-     $items = array('2.jpg');
-     foreach ($items as $item)
-     {
-       echo "<img src='Images/$item' width = 50px height = 50px alt='$item'>";
-       // code...
-     }
- ?>
+<h1>function</h1>
+<?php
+    $items = array('2.jpg', '3.jpg');
+    dislay_image_list($items);
+?>

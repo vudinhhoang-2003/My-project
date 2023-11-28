902183391cb9e557dda80de6ec91553233942d31
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-11-28 18:52:00 +0700

    function slide 20

---
+++ PHP/function.php
@@ -0,0 +1,18 @@
+<?php
+    $items = array('1.jpg', '2.jpg', '3.jpg');
+    foreach ($items as $item)
+    {
+      echo "<img src='Images/$item' width = 50px height = 50px alt='$item'>";
+      // code...
+    }
+ ?>
+<h2> Vũ Đình Hoàng - 2121050409</h2>
+ <h1>function</h1>
+ <?php
+     $items = array('2.jpg');
+     foreach ($items as $item)
+     {
+       echo "<img src='Images/$item' width = 50px height = 50px alt='$item'>";
+       // code...
+     }
+ ?>

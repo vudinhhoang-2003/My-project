4f888e0f77b33c69ec0b98c5f79b4b2e6da13f4e
vudinhhoang-2003 <hoangvudinh061@gmail.com>
2023-12-06 15:09:11 +0700

    update insert

--- PHP/insert.php
+++ PHP/insert.php
@@ -2,9 +2,16 @@
     // connect to the database
     require 'connect.php';
     // connect sql to insert data
-    $sql = "INSERT INTO flights (Origin, Destination, Duration) VALUES ('Hà Nội', 'Paris', '4000')";
+    $sql = "INSERT INTO flights (Origin, Destination, Duration) VALUES ('Hà Nội', 'Đà Nẵng', '1000')";
     // run the sql query
-    $conn->query($sql);
+    if ($conn->query($sql) === TRUE)
+    { // mo if 1
+      echo "New flight created successfully";
+    } // dong if 1
+    else
+    {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
     // close the connection
     $conn->close();
 ?>
